0089d9fa525ca9c3188a234f496f8f323ef11bf2
Valentin <vall-ball@yandex.ru>
2025-03-24 12:03:03 +0700

    The first commit 24.03.2025

---
+++ src/main/java/ru/vallball/forum04/Forum04Application.java
@@ -0,0 +1,13 @@
+package ru.vallball.forum04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forum04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Forum04Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vallball/forum04/Forum04ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vallball.forum04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forum04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
